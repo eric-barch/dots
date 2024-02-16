db93a29892e8e52922e5ef359e376e75e83e6fe9
Eric Barch <eric.michael.barch@gmail.com>
2024-02-16 15:05:17 -0500

    nvchad: remember folds on save

--- nvchad/init.lua
+++ nvchad/init.lua
@@ -1,2 +1,8 @@
 -- Set relative line numbers
 vim.opt.relativenumber = true
+
+-- Auto save folds when leaving a buffer
+vim.cmd([[autocmd BufWinLeave * mkview]])
+
+-- Auto load folds when entering a buffer
+vim.cmd([[autocmd BufWinEnter * silent! loadview]])
