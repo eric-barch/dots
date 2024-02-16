077d55481d31447630a6d56ccf5f626e2f3cc7f3
Eric Barch <eric.michael.barch@gmail.com>
2024-02-16 15:37:39 -0500

    fix: saving folds does not interfere with renamer.lua

--- nvchad/init.lua
+++ nvchad/init.lua
@@ -1,8 +1,8 @@
 -- Set relative line numbers
 vim.opt.relativenumber = true
 
--- Auto save folds when leaving a buffer
-vim.cmd([[autocmd BufWinLeave * mkview]])
+-- Auto save folds when leaving a buffer, but only for buffers associated with a file
+vim.cmd([[autocmd BufWinLeave * if !empty(expand('%')) && &buftype == '' | mkview | endif]])
 
--- Auto load folds when entering a buffer
-vim.cmd([[autocmd BufWinEnter * silent! loadview]])
+-- Auto load folds when entering a buffer, but only for buffers associated with a file
+vim.cmd([[autocmd BufWinEnter * if !empty(expand('%')) && &buftype == '' | silent! loadview | endif]])
