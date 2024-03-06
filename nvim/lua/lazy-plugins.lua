cc11f6aab82a135f5836a1e719bc1b40fb43870f
Eric Barch <eric.michael.barch@gmail.com>
2024-03-06 11:04:41 -0500

    nvim: Add autoformatting

--- nvim/lua/lazy-plugins.lua
+++ nvim/lua/lazy-plugins.lua
@@ -56,6 +56,48 @@ require('lazy').setup({
     },
   },
 
+	{
+    -- Autoformat
+		"stevearc/conform.nvim",
+    -- Recommended config for conform.nvim with LazyVim
+	  -- See https://github.com/stevearc/conform.nvim/blob/master/doc/recipes.md#lazy-loading-with-lazynvim
+		event = { "BufWritePre" },
+		cmd = { "ConformInfo" },
+		keys = {
+			{
+				"<leader>f",
+				function()
+					require("conform").format({ async = true, lsp_fallback = true })
+				end,
+				mode = "",
+				desc = "Format buffer",
+			},
+		},
+		-- Everything in opts is passed to setup()
+		opts = {
+			formatters_by_ft = {
+				lua = { "stylua" },
+				python = { "isort", "black" },
+				javascript = { { "prettierd", "prettier" } },
+				javascriptreact = { { "prettierd", "prettier" } },
+				typescript = { { "prettierd", "prettier" } },
+				typescriptreact = { { "prettierd", "prettier" } },
+			},
+			-- Set up format-on-save
+			format_on_save = { timeout_ms = 500, lsp_fallback = true },
+			-- Customize formatters
+			formatters = {
+				shfmt = {
+					prepend_args = { "-i", "2" },
+				},
+			},
+		},
+		init = function()
+			-- This is the the place to set formatexpr, for potential future use.
+			vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
+		end,
+	},
+
   {
     -- Autoclose brackets, quotes, etc.
     'windwp/nvim-autopairs',
