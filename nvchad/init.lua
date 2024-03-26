c22c9c0b2bfd5b526234e15c89269f7d25861c81
Eric Barch <eric.michael.barch@gmail.com>
2024-03-26 18:11:35 -0400

    nvchad: Remove nvchad config

--- nvchad/init.lua
+++
@@ -1,8 +0,0 @@
--- Set relative line numbers
-vim.opt.relativenumber = true
-
--- Auto save folds when leaving a buffer, but only for buffers associated with a file
-vim.cmd([[autocmd BufWinLeave * if !empty(expand('%')) && &buftype == '' | mkview | endif]])
-
--- Auto load folds when entering a buffer, but only for buffers associated with a file
-vim.cmd([[autocmd BufWinEnter * if !empty(expand('%')) && &buftype == '' | silent! loadview | endif]])
