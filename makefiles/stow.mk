839743a3cbb27fa42fffa2994e65359dbc57a927
Eric Barch <eric.michael.barch@gmail.com>
2024-04-14 14:22:52 -0400

    feat: Stow script creates XDG_CONFIG_HOME directories before symlinking

--- makefiles/stow.mk
+++ makefiles/stow.mk
@@ -6,12 +6,16 @@ ifeq ($(ARCH),arm)
 	STOW_BIN := /opt/homebrew/bin/stow
 endif
 
+PACKAGES := stow zsh tmux alacritty git starship nvim
+
 .PHONY: link
 link:
-	$(STOW_BIN) stow
-	$(STOW_BIN) zsh
-	$(STOW_BIN) tmux
-	$(STOW_BIN) alacritty
-	$(STOW_BIN) git
-	$(STOW_BIN) starship
-	$(STOW_BIN) nvim
+	# Create directories in XDG_CONFIG_HOME for each package
+	$(foreach pkg,$(PACKAGES),\
+		mkdir -p $(XDG_CONFIG_HOME)/$(pkg);\
+	)
+
+	# Stow each package into its respective directory
+	$(foreach pkg,\
+		$(PACKAGES),$(STOW_BIN) -t $(XDG_CONFIG_HOME)/$(pkg) $(pkg);\
+	)
