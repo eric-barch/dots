c6c3d5061553e86f47632eaddbddcbd88ec31d8d
Eric Barch <ericbarch@Erics-MacBook-Pro.local>
2024-04-14 16:21:31 -0400

    feat: Add alias.zsh

---
+++ zsh/alias.zsh
@@ -0,0 +1,18 @@
+# ls
+alias l="ls -FoG"
+alias ll="ls -FoGa"
+
+# git
+alias g="git"
+
+# lazygit
+alias lg="lazygit"
+
+# neovim
+alias nv="nvim"
+
+# tmux
+alias tn="tmux new"
+alias ta="tmux attach"
+alias td="tmux detach"
+alias tw="tmux new-session -A -s workspace"
