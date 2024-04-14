7d2b64cac2b8990a3d3a6ea7a43c3e9cebf8fc96
Eric Barch <eric.michael.barch@gmail.com>
2024-04-14 13:10:16 -0400

    feat: Add packages

--- makefiles/pkgs.mk
+++ makefiles/pkgs.mk
@@ -1,13 +1,30 @@
+ARCH := $(shell uname -p)
+ifeq ($(ARCH),i386)
+	BREW_PREFIX := /usr/local/
+	BREW_BIN := /usr/local/bin/brew
+endif
+ifeq ($(ARCH),arm)
+	BREW_PREFIX := /opt/homebrew
+	BREW_BIN := /opt/homebrew/bin/brew
+endif
+
 .PHONY: pkgs_all
-pkgs_all:
-	$(MAKE) pkgs_core
-	$(MAKE) pkgs_apps
+pkgs_all: pkgs_core pkgs_apps pkgs_fzf
+
+$(BREW_BIN):
+	[ -f $(BREW_BIN) ] || \
+		/bin/bash -c "$$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 
 .PHONY: pkgs_core
-	pkgs_core:
-		$(MAKE) pkgs_antidote
-		$(MAKE) pkgs_core
+pkgs_core: $(BREW_BIN)
+	$(MAKE) pkgs_antidote
+	$(BREW_BIN) bundle --file=homebrew/Brewfile-core
 
 .PHONY: pkgs_apps
-	pkgs_apps:
-		$(MAKE) pkgs_apps
+pkgs_apps: $(BREW_BIN)
+	$(BREW_BIN) bundle --file=homebrew/Brewfile-apps
+
+.PHONY: pkgs_fzf
+pkgs_fzf: $(BREW_BIN)
+	# Brew-specific extra steps
+	[ -f $(HOME)/.fzf.zsh ] || ($(BREW_PREFIX)/opt/fzf/install; rm ~/.fzf.bash)
