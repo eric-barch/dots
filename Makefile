5d55d4b40cb32ff90726feccd3ed0f38fb32bcdc
Eric Barch <eric.michael.barch@gmail.com>
2024-04-15 16:36:22 -0400

    fix: Base XDG_CONFIG_HOME directories on pwd instead of set list

--- Makefile
+++ Makefile
@@ -1,11 +1,6 @@
 include makefiles/pkgs.mk
 include makefiles/link.mk
 
-# Check XDG_CONFIG_HOME and set if not defined
-XDG_CONFIG_HOME ?= $(HOME)/.config
-
-export XDG_CONFIG_HOME
-
 # Initialize git submodules
 submodules:
 	git submodule update --init
--- makefiles/link.mk
+++ makefiles/link.mk
@@ -6,16 +6,22 @@ ifeq ($(ARCH),arm)
 	STOW_BIN := /opt/homebrew/bin/stow
 endif
 
-PACKAGES := stow zsh tmux alacritty git starship nvim
+# Set XDG_CONFIG_HOME if not already defined
+XDG_CONFIG_HOME ?= $(HOME)/.config
+export XDG_CONFIG_HOME
+
+# List directories to copy into XDG_CONFIG_HOME
+DIRS := $(notdir $(patsubst %/,%,$(wildcard */)))
+PACKAGES := $(filter-out .git makefiles, $(DIRS))
 
 .PHONY: link
 link:
-	# Create directories in XDG_CONFIG_HOME for each package
+	# Create corresponding directories in XDG_CONFIG_HOME
 	$(foreach pkg,$(PACKAGES),\
 		mkdir -p $(XDG_CONFIG_HOME)/$(pkg);\
 	)
 
-	# Stow each package into its respective directory
-	$(foreach pkg,\
-		$(PACKAGES),$(STOW_BIN) -t $(XDG_CONFIG_HOME)/$(pkg) $(pkg);\
+	# Symlink directories in pwd to corresponding in XDG_CONFIG_HOME
+	$(foreach pkg,$(PACKAGES),\
+		$(STOW_BIN) -t $(XDG_CONFIG_HOME)/$(pkg) $(pkg);\
 	)
