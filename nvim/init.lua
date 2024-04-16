80991e8f07d23ed804656c03f09f42666284f018
Eric Barch <eric.michael.barch@gmail.com>
2024-04-16 02:02:52 -0400

    fix: Remove extraneous comments from init.lua

--- nvim/init.lua
+++ nvim/init.lua
@@ -3,20 +3,11 @@
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
--- Set Nerd Font to true
 vim.g.have_nerd_font = true
 
--- [[ Setting options ]]
 require 'options'
-
--- [[ Basic Keymaps ]]
 require 'keymaps'
-
--- [[ Install `lazy.nvim` plugin manager ]]
 require 'lazy-bootstrap'
-
--- [[ Configure and install plugins ]]
 require 'lazy-plugins'
 
--- See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
