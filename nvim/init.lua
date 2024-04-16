9b40b004e205cb330da841bed9200cc2e8d02464
Eric Barch <eric.michael.barch@gmail.com>
2024-04-16 01:24:55 -0400

    fix: Clean up init.lua extraneous comments

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,6 +1,5 @@
 -- Set <space> as the leader key
 -- See `:help mapleader`
---  NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
@@ -19,5 +18,5 @@ require 'lazy-bootstrap'
 -- [[ Configure and install plugins ]]
 require 'lazy-plugins'
 
--- The line beneath this is called `modeline`. See `:help modeline`
+-- See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
