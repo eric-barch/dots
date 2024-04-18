5a458705260073b6c3ac3f1bafdf8355b0ba91da
Eric Barch <eric.michael.barch@gmail.com>
2024-04-18 10:33:03 -0400

    style(nvim): Style functions to max width 80, correct case of keymap descriptions

--- nvim/lua/keymaps.lua
+++ nvim/lua/keymaps.lua
@@ -6,18 +6,34 @@ vim.opt.hlsearch = true
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 
 -- Diagnostic keymaps.
-vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous [D]iagnostic message' })
-vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next [D]iagnostic message' })
-vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Show diagnostic [E]rror messages' })
-vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
+vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, {
+  desc = 'Go to previous [d]iagnostic message',
+})
+vim.keymap.set('n', ']d', vim.diagnostic.goto_next, {
+  desc = 'Go to next [d]iagnostic message',
+})
+vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, {
+  desc = 'Show diagnostic [e]rror messages',
+})
+vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, {
+  desc = 'Open diagnostic [q]uickfix list',
+})
 
 -- Exit builtin terminal. Default is <C-\><C-n>.
 -- NOTE: This won't work in all terminal emulators/tmux/etc.
-vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
+vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', {
+  desc = 'Exit terminal mode',
+})
 
 -- Improve wrapped line navigation.
-vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
-vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
+vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", {
+  expr = true,
+  silent = true,
+})
+vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", {
+  expr = true,
+  silent = true,
+})
 
 -- Set last pane width to 80 on vertical split.
 vim.keymap.set('n', '<C-w>v', function()
@@ -35,6 +51,7 @@ vim.keymap.set('n', '<C-w>v', function()
   vim.cmd('vertical resize ' .. (column_width + target_text_width))
   vim.cmd 'wincmd l'
 end, {
+  desc = 'Split window [v]ertically',
   -- Prevent keymap from propagating.
   noremap = true,
   -- Suppress output in command line.
@@ -43,10 +60,18 @@ end, {
 
 -- Keybinds to make split navigation easier.
 -- See `:help wincmd` for a list of all window commands.
-vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to left split' })
-vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to right split' })
-vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to lower split' })
-vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to upper split' })
+vim.keymap.set('n', '<C-h>', '<C-w><C-h>', {
+  desc = 'Move focus to left split',
+})
+vim.keymap.set('n', '<C-l>', '<C-w><C-l>', {
+  desc = 'Move focus to right split',
+})
+vim.keymap.set('n', '<C-j>', '<C-w><C-j>', {
+  desc = 'Move focus to lower split',
+})
+vim.keymap.set('n', '<C-k>', '<C-w><C-k>', {
+  desc = 'Move focus to upper split',
+})
 
 -- Basic Autocommands.
 -- See `:help lua-guide-autocommands`.
@@ -55,7 +80,9 @@ vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to upper split' 
 -- See `:help vim.highlight.on_yank()`.
 vim.api.nvim_create_autocmd('TextYankPost', {
   desc = 'Highlight when yanking (copying) text',
-  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', {
+    clear = true,
+  }),
   callback = function()
     vim.highlight.on_yank()
   end,
