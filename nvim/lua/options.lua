24955555f9155eb90957d5ff26bfbe6afd48d7b4
Eric Barch <eric.michael.barch@gmail.com>
2024-05-31 10:36:17 -0400

    fix(nvim): Set `vim.opt.splitright` to `true`

--- nvim/lua/options.lua
+++ nvim/lua/options.lua
@@ -43,6 +43,7 @@ vim.opt.updatetime = 250
 vim.opt.timeoutlen = 300
 
 -- Configure how new splits should be opened.
+vim.opt.splitright = true
 vim.opt.splitbelow = true
 
 -- Sets how neovim will display certain whitespace characters in the editor.
