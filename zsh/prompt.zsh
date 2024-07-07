f46d0b8c8263cf383f71be8930333c3d09a1dfce
Eric Barch <eric.michael.barch@gmail.com>
2024-07-07 14:05:11 -0400

    feat(oh-my-posh): Implement oh-my-posh prompt

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -1,1 +1,5 @@
-command -v starship >/dev/null && eval "$(starship init zsh)"
+# Select one of Starship or Oh My Posh. Note: Oh My Posh does not support Apple 
+# Terminal.
+
+# eval "$(starship init zsh)"
+eval "$(oh-my-posh init zsh --config $XDG_CONFIG_HOME/oh-my-posh/oh-my-posh.toml)"
