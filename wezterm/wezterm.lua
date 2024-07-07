3efc21e14ef1ee65bb0deca72466822da4d4d0be
Eric Barch <eric.michael.barch@gmail.com>
2024-07-07 16:26:57 -0400

    refactor(wezterm): Clean up config

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,20 +1,20 @@
 local wezterm = require("wezterm")
 local config = {}
 
-config.color_scheme = "onedark"
+config.color_scheme = "catppuccin-mocha"
 config.cursor_blink_rate = 0
 config.enable_tab_bar = false
-config.font = wezterm.font("CaskaydiaCove Nerd Font")
+config.font = wezterm.font("CaskaydiaCove Nerd Font", { weight = "Regular" })
 config.font_size = 14
-config.hide_tab_bar_if_only_one_tab = true
 config.line_height = 1.2
+config.hide_tab_bar_if_only_one_tab = true
 config.window_close_confirmation = "NeverPrompt"
 config.window_decorations = "RESIZE"
 config.window_padding = {
-	left = 5,
-	right = 5,
-	top = 5,
-	bottom = 5,
+	left = 2,
+	right = 2,
+	top = 2,
+	bottom = 2,
 }
 
 return config
