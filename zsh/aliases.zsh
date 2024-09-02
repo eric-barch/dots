f546367c48ecbb326f01ad912953378752f98ccb
Eric Barch <eric.michael.barch@gmail.com>
2024-09-02 17:58:40 -0400

    refactor(zsh): Clean up aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,15 +1,13 @@
 # clear
 alias c="clear"
 
-# esp idf
+# ESP-IDF
+# https://docs.espressif.com/projects/esp-idf/en/stable/esp32/get-started/linux-macos-setup.html#step-4-set-up-the-environment-variables
 alias esp-env=". $HOME/esp/esp-idf/export.sh"
 
 # ls
 alias l="ls -FoG"
 alias ll="ls -FoGa"
-
-# git
-alias g="git"
 
 # lazygit
 alias lg="lazygit"
@@ -21,5 +19,3 @@ alias nv="nvim"
 alias tl="tmux ls"
 alias tn="tmux new"
 alias ta="tmux attach"
-alias td="tmux detach"
-alias tw="tmux new-session -A -s workspace"
