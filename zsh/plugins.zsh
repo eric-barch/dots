c9824290be8189c1d86c240e7f4b3b7d5df3ae16
Eric Barch <eric.michael.barch@gmail.com>
2024-09-02 18:02:00 -0400

    refactor(zsh): Move plugins into own directory

--- zsh/plugins.zsh
+++ zsh/plugins.zsh
@@ -1,5 +1,7 @@
+# Antidote ZSH Plugin Manager
+# https://github.com/mattmc3/antidote?tab=readme-ov-file#usage
 ANTIDOTE="$XDG_CONFIG_HOME/antidote/antidote.zsh"
 if [ -f "$ANTIDOTE" ]; then
   source "$ANTIDOTE"
-  antidote load "$XDG_CONFIG_HOME/zsh/antidote_plugins.txt"
+  antidote load "$XDG_CONFIG_HOME/zsh/plugins/antidote_plugins.txt"
 fi
---
+++ zsh/plugins/antidote_plugins.zsh
@@ -0,0 +1,4 @@
+fpath+=( $HOME/Library/Caches/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-syntax-highlighting )
+source $HOME/Library/Caches/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-syntax-highlighting/zsh-syntax-highlighting.plugin.zsh
+fpath+=( $HOME/Library/Caches/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-autosuggestions )
+source $HOME/Library/Caches/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-autosuggestions/zsh-autosuggestions.plugin.zsh
