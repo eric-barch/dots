e2cd96aa132dbc9f55b168121080151184b79456
Eric Barch <eric.michael.barch@gmail.com>
2024-09-02 17:58:18 -0400

    refactor(zsh): Refactor `prompt.zsh`

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -1,2 +1,4 @@
-# Note: Oh My Posh does not support Apple Terminal.
+# Oh My Posh
+# https://ohmyposh.dev/docs/installation/prompt
+# https://ohmyposh.dev/docs/installation/customize
 eval "$(oh-my-posh init zsh --config $XDG_CONFIG_HOME/oh-my-posh/oh-my-posh.toml)"
