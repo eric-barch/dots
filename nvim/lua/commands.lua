0f3fb1fdb94545fd492fc5ef22e42e1cb9fd58de
Eric Barch <eric.michael.barch@gmail.com>
2024-09-15 23:31:32 -0400

    feat(nvim): Start with `$BASE16_THEME` colorscheme if set

--- nvim/lua/commands.lua
+++ nvim/lua/commands.lua
@@ -1,7 +1,7 @@
 -- Run commands
 -- See `:help vim.cmd`
 
--- Set colorscheme
-vim.cmd.colorscheme 'base16-gruvbox-dark-hard'
+-- Set colorscheme. Uses $BASE16_THEME env variable if set.
+require('base16-colorscheme').setup()
 
 -- vim: ts=2 sts=2 sw=2 et
