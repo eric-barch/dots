d17bfc2f2cf2770ddb287430b5668cef1765ba51
Eric Barch <eric.michael.barch@gmail.com>
2024-09-21 11:42:09 -0400

    refactor(nvim): Update `lazy-bootstrap` to follow docs

--- nvim/lua/lazy-bootstrap.lua
+++ nvim/lua/lazy-bootstrap.lua
@@ -2,12 +2,19 @@
 -- See `:help lazy.nvim`
 
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
-
-if not vim.loop.fs_stat(lazypath) then
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
   local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
-  vim.fn.system { 'git', 'clone', '--filter=blob:none', '--branch=stable', lazyrepo, lazypath }
+  local out = vim.fn.system { 'git', 'clone', '--filter=blob:none', '--branch=stable', lazyrepo, lazypath }
+  if vim.v.shell_error ~= 0 then
+    vim.api.nvim_echo({
+      { 'Failed to clone lazy.nvim:\n', 'ErrorMsg' },
+      { out, 'WarningMsg' },
+      { '\nPress any key to exit...' },
+    }, true, {})
+    vim.fn.getchar()
+    os.exit(1)
+  end
 end
-
 vim.opt.rtp:prepend(lazypath)
 
 -- vim: ts=2 sts=2 sw=2 et
