b8b48c070ef891c2d4008b03a7d8a2ef6d738182
Eric Barch <eric.michael.barch@gmail.com>
2024-10-14 23:46:44 -0400

    fix(zsh): Set `$XDG_RUNTIME_DIR` to `/tmp`

--- zsh/env.zsh
+++ zsh/env.zsh
@@ -4,13 +4,13 @@ export EDITOR=nvim
 
 # XDG environment variables
 # https://specifications.freedesktop.org/basedir-spec/latest/index.html#variables
-export XDG_RUNTIME_DIR="${XDG_RUNTIME_DIR:=/run/user/$UID}"
 export XDG_DATA_HOME="${XDG_DATA_HOME:-$HOME/.local/share}"
 export XDG_CONFIG_HOME="${XDG_CONFIG_HOME:-$HOME/.config}"
 export XDG_STATE_HOME="${XDG_STATE_HOME:-$HOME/.local/state}"
 export XDG_CACHE_HOME="${XDG_CACHE_HOME:-$HOME/.cache}"
 export XDG_DATA_DIRS="${XDG_DATA_DIRS:-/usr/local/share:/usr/share}"
 export XDG_CONFIG_DIRS="${XDG_CONFIG_DIRS:-/etc/xdg}"
+export XDG_RUNTIME_DIR="${XDG_RUNTIME_DIR:-/tmp}"
 
 # XDG recommends this location for executables, but does not actually specify 
 # an environment variable. Thus, no XDG prefix.
