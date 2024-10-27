1803cea2fd0e5c5e4f55e0b546496ec377e89da1
Eric Barch <eric.michael.barch@gmail.com>
2024-10-26 23:01:19 -0400

    refactor(nvim): Move luasnip setup into separate file

--- nvim/lua/plugins/cmp.lua
+++ nvim/lua/plugins/cmp.lua
@@ -1,51 +1,18 @@
--- Autocompletion.
--- See `:help cmp`.
+-- Autocompletion engine
+-- See `:help cmp`
 
 return {
   {
     'hrsh7th/nvim-cmp',
     event = 'InsertEnter',
     dependencies = {
-      {
-        -- Snippet engine and associated nvim-cmp source
-        'L3MON4D3/LuaSnip',
-        build = (function()
-          -- Build step required for regex support in snippets. Not supported in
-          -- many Windows environments. Remove this condition to enable on
-          -- Windows.
-          if vim.fn.has 'win32' == 1 or vim.fn.executable 'make' == 0 then
-            return
-          end
-
-          return 'make install_jsregexp'
-        end)(),
-        dependencies = {
-          -- `friendly-snippets` contains a variety of premade snippets.
-          --  See the README about individual language/framework/plugin snippets:
-          --  https://github.com/rafamadriz/friendly-snippets
-          -- {
-          --   'rafamadriz/friendly-snippets',
-          --   config = function()
-          --     require('luasnip.loaders.from_vscode').lazy_load()
-          --   end,
-          -- },
-        },
-      },
-
-      -- Completion source for luasnip.
-      'saadparwaiz1/cmp_luasnip',
-
-      -- Completion source for nvim builtin LSP.
-      'hrsh7th/cmp-nvim-lsp',
-
-      -- Completion source for filesystem paths.
-      'hrsh7th/cmp-path',
+      'saadparwaiz1/cmp_luasnip', -- Completion source for LuaSnip
+      'hrsh7th/cmp-nvim-lsp', -- Completion source for built in LSP
+      'hrsh7th/cmp-path', -- Completion source for filesystem paths
     },
-    config = function()
+    opts = function()
       local cmp = require 'cmp'
       local luasnip = require 'luasnip'
-
-      luasnip.config.setup {}
 
       cmp.setup {
         snippet = {
@@ -53,12 +20,7 @@ return {
             luasnip.lsp_expand(args.body)
           end,
         },
-
         completion = { completeopt = 'menu,menuone,noinsert' },
-
-        -- Why these mappings were chosen: `:help ins-completion`.
-        -- For more advanced Luasnip keymaps:
-        -- https://github.com/L3MON4D3/LuaSnip?tab=readme-ov-file#keymaps
         mapping = cmp.mapping.preset.insert {
           ['<C-n>'] = cmp.mapping.select_next_item(),
           ['<C-p>'] = cmp.mapping.select_prev_item(),
@@ -66,13 +28,11 @@ return {
           ['<C-f>'] = cmp.mapping.scroll_docs(4),
           ['<C-y>'] = cmp.mapping.confirm { select = true },
           ['<C-Space>'] = cmp.mapping.complete {},
-
           ['<C-l>'] = cmp.mapping(function()
             if luasnip.expand_or_locally_jumpable() then
               luasnip.expand_or_jump()
             end
           end, { 'i', 's' }),
-
           ['<C-h>'] = cmp.mapping(function()
             if luasnip.locally_jumpable(-1) then
               luasnip.jump(-1)
@@ -80,8 +40,8 @@ return {
           end, { 'i', 's' }),
         },
         sources = {
-          { name = 'nvim_lsp' },
           { name = 'luasnip' },
+          { name = 'nvim_lsp' },
           { name = 'path' },
         },
       }
---
+++ nvim/lua/plugins/luasnip.lua
@@ -0,0 +1,19 @@
+-- Snippet engine
+-- See `:help luasnip-basics`
+
+return {
+  {
+    'L3MON4D3/LuaSnip', -- Snippet engine and associated nvim-cmp source
+    build = (function() -- Build step required for regex support in snippets
+      return 'make install_jsregexp'
+    end)(),
+    dependencies = {
+      'rafamadriz/friendly-snippets', -- Premade snippets.
+      config = function()
+        require('luasnip.loaders.from_vscode').lazy_load()
+      end,
+    },
+  },
+}
+
+-- vim: ts=2 sts=2 sw=2 et
