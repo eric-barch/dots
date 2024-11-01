a4b11b2c950985a70c6c3690b140b26ecd6f689b
Eric Barch <eric.michael.barch@gmail.com>
2024-10-31 21:09:09 -0400

    feat(nvim): Improve `gitsigns.nvim`

--- nvim/lua/plugins/gitsigns.lua
+++ nvim/lua/plugins/gitsigns.lua
@@ -1,17 +1,39 @@
--- Add git signs to the gutter.
--- See `:help gitsigns`.
+-- Git signs in the gutter
+-- See `:help gitsigns`
 
 return {
   {
     'lewis6991/gitsigns.nvim',
     opts = {
       signs = {
-        add = { text = '+' },
-        change = { text = '~' },
+        add = { text = '▌' },
+        change = { text = '▌' },
         delete = { text = '_' },
         topdelete = { text = '‾' },
-        changedelete = { text = '~' },
+        changedelete = { text = '▌' },
+        untracked = { text = '▌' },
       },
+      signs_staged = {
+        add = { text = '▐' },
+        change = { text = '▐' },
+        delete = { text = '_' },
+        topdelete = { text = '‾' },
+        changedelete = { text = '▐' },
+        untracked = { text = '▐' },
+      },
+      signs_staged_enable = true,
+      signcolumn = true,
+      current_line_blame = true,
+      current_line_blame_opts = {
+        virt_text = true,
+        virt_text_pos = 'eol',
+        delay = 300,
+        ignore_whitespace = false,
+        virt_text_priority = 100,
+        use_focus = true,
+      },
+      current_line_blame_formatter = '   <author>, <author_time:%R> - <summary>',
+      current_line_blame_formatter_nc = '   Not committed',
     },
   },
 }
