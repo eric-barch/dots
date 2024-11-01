de76934b57d0255fcfa9dbfe8356c5d6ce1f1d3e
Eric Barch <eric.michael.barch@gmail.com>
2024-10-31 21:09:37 -0400

    style(nvim): Clean up `indent-blanklines.nvim`

--- nvim/lua/plugins/indent_line.lua
+++ nvim/lua/plugins/indent_line.lua
@@ -1,9 +1,16 @@
+-- Indentation guide lines
+-- See `:help ibl`
+
 return {
-  { -- Add indentation guides even on blank lines
+  {
     'lukas-reineke/indent-blankline.nvim',
-    -- Enable `lukas-reineke/indent-blankline.nvim`
-    -- See `:help ibl`
     main = 'ibl',
-    opts = {},
+    opts = {
+      indent = {
+        char = '▏',
+      },
+    },
   },
 }
+
+-- vim: ts=2 sts=2 sw=2 et
