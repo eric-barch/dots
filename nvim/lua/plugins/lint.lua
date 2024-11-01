614cca47bc4b92c4c085addbbc923f744c5b236b
Eric Barch <eric.michael.barch@gmail.com>
2024-10-31 23:07:26 -0400

    refactor(nvim): Clean up `nvim-lint`

--- nvim/lua/plugins/lint.lua
+++ nvim/lua/plugins/lint.lua
@@ -1,20 +1,17 @@
+-- Pass linter output to `vim.diagnostic` module
+-- See `:help lint`
+
 return {
-  { -- Linting
+  {
     'mfussenegger/nvim-lint',
     event = { 'BufReadPre', 'BufNewFile' },
     config = function()
       local lint = require 'lint'
-      lint.linters_by_ft = {
-        markdown = { 'markdownlint' },
-      }
+      -- Allow other plugins to add linters to require('lint').linters_by_ft
+      lint.linters_by_ft = lint.linters_by_ft or {}
 
-      -- To allow other plugins to add linters to require('lint').linters_by_ft,
-      -- instead set linters_by_ft like this:
-      -- lint.linters_by_ft = lint.linters_by_ft or {}
-      -- lint.linters_by_ft['markdown'] = { 'markdownlint' }
-      --
-      -- However, note that this will enable a set of default linters,
-      -- which will cause errors unless these tools are available:
+      -- This enables a set of default linters, which will cause errors unless
+      -- these tools are available:
       -- {
       --   clojure = { "clj-kondo" },
       --   dockerfile = { "hadolint" },
@@ -27,21 +24,16 @@ return {
       --   terraform = { "tflint" },
       --   text = { "vale" }
       -- }
-      --
-      -- You can disable the default linters by setting their filetypes to nil:
-      -- lint.linters_by_ft['clojure'] = nil
-      -- lint.linters_by_ft['dockerfile'] = nil
-      -- lint.linters_by_ft['inko'] = nil
-      -- lint.linters_by_ft['janet'] = nil
-      -- lint.linters_by_ft['json'] = nil
-      -- lint.linters_by_ft['markdown'] = nil
-      -- lint.linters_by_ft['rst'] = nil
-      -- lint.linters_by_ft['ruby'] = nil
-      -- lint.linters_by_ft['terraform'] = nil
-      -- lint.linters_by_ft['text'] = nil
 
-      -- Create autocommand which carries out the actual linting
-      -- on the specified events.
+      -- So disable the unwanted ones by setting filetypes to nil:
+      lint.linters_by_ft['clojure'] = nil
+      lint.linters_by_ft['inko'] = nil
+      lint.linters_by_ft['janet'] = nil
+      lint.linters_by_ft['markdown'] = nil
+      lint.linters_by_ft['rst'] = nil
+      lint.linters_by_ft['ruby'] = nil
+      lint.linters_by_ft['terraform'] = nil
+
       local lint_augroup = vim.api.nvim_create_augroup('lint', { clear = true })
       vim.api.nvim_create_autocmd({ 'BufEnter', 'BufWritePost', 'InsertLeave' }, {
         group = lint_augroup,
@@ -52,3 +44,5 @@ return {
     end,
   },
 }
+
+-- vim: ts=2 sts=2 sw=2 et
