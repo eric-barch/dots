c1387004dc46183a007be0985131c713989507a1
Eric Barch <eric.michael.barch@gmail.com>
2025-10-10 23:34:09 -0400

    refactor(theme): Replace `theme` with `scheme`

--- zsh/env.zsh
+++ zsh/env.zsh
@@ -19,10 +19,6 @@ export BIN_HOME="${BIN_HOME:-$HOME/.local/bin}"
 # Move zsh config location from default (home) to XDG
 export ZDOTDIR="${ZDOTDIR:-$XDG_CONFIG_HOME/zsh}"
 
-# Base16 theme
-# https://github.com/chriskempson/base16-shell
-export BASE16_THEME="catppuccin-mocha"
-
 # pnpm
 export PNPM_HOME="${XDG_DATA_HOME}/pnpm"
 
--- zsh/theme.zsh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-theme "$BASE16_THEME"
