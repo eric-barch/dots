9eb686e2ce5c6b0473c19efa5c4e281e3fc48394
Eric Barch <eric.michael.barch@gmail.com>
2025-10-28 19:33:56 -0400

    style(nvim): Format `snacks`

--- nvim/lua/plugins/snacks.lua
+++ nvim/lua/plugins/snacks.lua
@@ -3,13 +3,10 @@
 
 return {
   {
-    "folke/snacks.nvim",
+    'folke/snacks.nvim',
     priority = 1000,
     lazy = false,
     opts = {
-      -- your configuration comes here
-      -- or leave it empty to use the default settings
-      -- refer to the configuration section below
       -- bigfile = { enabled = true },
       -- dashboard = { enabled = true },
       -- explorer = { enabled = true },
@@ -27,3 +24,4 @@ return {
 }
 
 -- vim: ts=2 sts=2 sw=2 et
+
