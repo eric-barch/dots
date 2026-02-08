d214728f540d437768a2955964920152f3e0dc3d
Eric Barch <eric.michael.barch@gmail.com>
2026-02-07 23:30:04 -0500

    refactor(nvim): align `lint.lua` w `lspconfig.lua`
    Preliminary. I don't think installation is working. If you were to put a
    new linter in a project's `.nvim.lua` and fire up nvim, it doesn't seem
    like it installs first try. It should.

--- nvim/lua/plugins/lint.lua
+++ nvim/lua/plugins/lint.lua
@@ -1,25 +1,45 @@
 -- Linting
 -- See `:help lint`
 
+local project = require 'utils.project'
+
+local default_linters = {
+  javascript = { 'eslint_d' },
+  javascriptreact = { 'eslint_d' },
+  json = { 'jsonlint' },
+  markdown = { 'markdownlint' },
+  typescript = { 'eslint_d' },
+  typescriptreact = { 'eslint_d' },
+}
+
+local function get_linters()
+  project.load_config()
+
+  if vim.g.project and vim.g.project.lint then
+    local linters = vim.g.project.lint
+
+    if type(linters) == 'table' then
+      return linters
+    end
+
+    return { linters }
+  end
+
+  return default_linters
+end
+
 return {
   {
     'mfussenegger/nvim-lint',
-    event = { 'BufReadPost', 'BufNewFile' },
     dependencies = {
       'williamboman/mason.nvim',
       'WhoIsSethDaniel/mason-tool-installer.nvim',
     },
     config = function()
-      local lint = require 'lint'
+      require('mason').setup()
 
-      lint.linters_by_ft = {
-        javascript = { 'eslint_d' },
-        javascriptreact = { 'eslint_d' },
-        json = { 'jsonlint' },
-        markdown = { 'markdownlint' },
-        typescript = { 'eslint_d' },
-        typescriptreact = { 'eslint_d' },
-      }
+      local lint = require 'lint'
+      lint.linters_by_ft = get_linters()
 
       local linters_to_install = {}
       for _, linters in pairs(lint.linters_by_ft) do
